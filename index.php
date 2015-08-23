2aaeb50fce91b712e327bc2a21bcf85c9e67560c
Igor Bodnar <ibodnar@amaxlab.ru>
2015-08-23 14:11:29 +0600

    updated to work in a preferred way (using yml config files)

--- index.php
+++ index.php
@@ -12,25 +12,9 @@ use AmaxLab\GitWebHook\Hook;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$options = array(
-    'sendEmails'          => true,
-    'sendEmailAuthor'     => true,
-    'mailRecipients'      => array(),
-    'allowedAuthors'      => '*',
-    'allowedHosts'        => '*',
-);
-
 $logger = new Logger('git-web-hook');
-$logger->pushHandler(new StreamHandler(__DIR__ . '/hook.log', Logger::WARNING));
+$logger->pushHandler(new StreamHandler(__DIR__ . 'log'. DIRECTORY_SEPARATOR.'hook.log', Logger::WARNING)); // or you can write logs to /var/log or somewhere else
 
-$hook = new Hook(__DIR__, $options, $logger);
-
-$hook
-    ->addRepository('git@github.com:amaxlab/git-web-hook.git', '/var/www/my_project_folder/web')
-    ->addBranch('master', array('git status', 'git reset --hard HEAD', 'git pull origin master'), '/var/www/my_project_folder/demo_subdomain') // commands executed on push to specified branch in /var/www/html/my_site/ folder
-    ->addBranch('production', 'git pull origin production');
-
-// or load from yml files
-$hook->loadRepos(__DIR__.'/repos.d/');
-
+$hook = new Hook(__DIR__, array(), $logger);
+$hook->loadConfig(__DIR__.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.yml'); // you can copy config.yml.dist to config.yml and edit it
 $hook->execute();
