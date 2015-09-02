579909a19dbbb0b078c6d1470ff56a9213274fe8
Egor Zyuskin <egor@zyuskin.ru>
2015-09-02 09:54:22 +0600

    Fix log saving directory

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 $logger = new Logger('git-web-hook');
-$logger->pushHandler(new StreamHandler(__DIR__ . 'log'. DIRECTORY_SEPARATOR.'hook.log', Logger::WARNING)); // or you can write logs to /var/log or somewhere else
+$logger->pushHandler(new StreamHandler(__DIR__ . DIRECTORY_SEPARATOR . 'log'. DIRECTORY_SEPARATOR.'hook.log', Logger::WARNING)); // or you can write logs to /var/log or somewhere else
 
 $hook = new Hook(__DIR__, array(), $logger);
 $hook->loadConfig(__DIR__.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.yml'); // you can copy config.yml.dist to config.yml and edit it
